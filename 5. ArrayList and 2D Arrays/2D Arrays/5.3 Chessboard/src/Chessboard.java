b9015ebb5f5cc2442b03b8003596bf373b8c537f
DjDarioGC <dario.iglesias101@alu.ulpgc.es>
2023-09-30 20:22:28 +0100

    Vigésimo quinto commit (Ajedrez)

--- 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
+++ 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
@@ -4,6 +4,24 @@ public class Chessboard
    {
       char[][] chessboard = new char[8][8];
 
-      /* Create and print a chessboard. */
+      for (int row = 0; row < 8; row++) {
+         for (int col = 0; col < 8; col++) {
+            if ((row + col) % 2 == 0) {
+               chessboard[row][col] = '\u25A1'; // Cuadro negro ('\u25A0')
+            } else {
+               chessboard[row][col] = '\u25A0'; // Cuadro blanco ('\u25A1')
+            }
+         }
+      }
+      for (int row = 0; row < 8; row++) {
+         for (int col = 0; col < 8; col++) {
+            System.out.print(chessboard[row][col]);
+         }
+         System.out.println(); // Nueva línea al final de cada fila
+      }
    }
 }
+
+
+
+
