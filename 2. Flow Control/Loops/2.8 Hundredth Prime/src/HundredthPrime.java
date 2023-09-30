53523ae257b2e55c824704b56d7007f5707c8eab
DjDarioGC <dario.iglesias101@alu.ulpgc.es>
2023-09-30 13:46:18 +0100

    Décimo tercer commit

--- 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
+++ 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
@@ -1,7 +1,24 @@
-public class HundredthPrime
-{
-   public static void main(String[] args)
-   {
-      /* Make a code that displays the 100th prime number */
+public class HundredthPrime {
+   public static void main(String[] args) {
+      int contadorPrimos = 0;
+      int numeroActual = 2;
+
+      while (contadorPrimos < 100) {
+         boolean esPrimo = true;
+         for (int i = 2; i * i <= numeroActual; i++) {
+            if (numeroActual % i == 0) {
+               esPrimo = false;
+               break;
+            }
+         }
+
+         if (esPrimo) {
+            contadorPrimos++;
+         }
+
+         numeroActual++;
+      }
+
+      System.out.println((numeroActual - 1));
    }
 }
